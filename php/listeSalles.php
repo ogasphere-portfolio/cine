77fb1e2245aeb4f41605addaab5479397b8f477b
olivier <olivier@oclock.io>
2021-06-21 09:30:01 +0200

    étape 1

--- php/listeSalles.php
+++ php/listeSalles.php
@@ -1,5 +1,12 @@
 <?php
     require 'includes/header.php';
+    // tableau contenant la liste des sallesde notre ciné
+    $rooms = [
+        'Athéna', // 0
+        'Dyonisos', // 1
+        'Hadès', // 2
+        'Zeus' // 3
+    ];
 ?>
 
 <section>
@@ -7,7 +14,12 @@
         Les salles de notre cinéma
     </h2>
 
-    
+    <ul>
+        <li>
+            <?php echo $rooms[0]; ?>
+        </li>
+    </ul>
+
 </section>
 
 <?php
