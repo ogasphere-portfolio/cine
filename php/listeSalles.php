0c857df2bbf4f42ad3ffede5b1049fe4a438160b
olivier <olivier@oclock.io>
2021-06-21 10:22:46 +0200

    fin exo

--- php/listeSalles.php
+++ php/listeSalles.php
@@ -1,6 +1,6 @@
 <?php
     require 'includes/header.php';
-    // tableau contenant la liste des sallesde notre ciné
+    // tableau contenant la liste des salles de notre ciné
     $rooms = [
         'Athéna', // 0
         'Dyonisos', // 1
@@ -15,9 +15,47 @@
     </h2>
 
     <ul>
-        <li>
-            <?php echo $rooms[0]; ?>
-        </li>
+        <?php
+        // début de la boucle
+        for ($index = 0; $index <= 3 ; $index++) { 
+            ?>
+            <li>
+                <?php echo $rooms[$index]; ?>
+            </li>
+            <?php
+        }
+        // fin de la boucle
+        ?>
+    </ul>
+    
+    <hr>
+    
+    <ul>
+        <?php
+        // début de la boucle
+        // syntaxe alternative pour écrire un for
+        for ($index = 0; $index <= 3 ; $index++) :
+            ?>
+            <li>
+                <?php echo $rooms[$index]; ?>
+            </li>
+            <?php
+        endfor;
+        // fin de la boucle
+        ?>
+
+    </ul>
+
+    <hr>
+
+    <ul>
+        <?php
+            // début de la boucle
+            for ($index = 0; $index <= 3 ; $index++) {
+                echo '<li class="list__item">' . $rooms[$index] . "</li>";
+            }
+            // fin de la boucle
+        ?>
     </ul>
 
 </section>
